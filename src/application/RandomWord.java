224db5319912dd0ed189b327f705e1135f01df16
earl-stephens <stephensearl@hotmail.com>
2022-01-01 15:32:17 -0700

    Create needed classes and method stubs

---
+++ src/application/RandomWord.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.Random;
+
+public class RandomWord {
+
+	String chosenWord;
+	Random random = new Random();
+	
+	private void selectWord() {
+		int selection = random.nextInt(10);
+		
+	}
+}
